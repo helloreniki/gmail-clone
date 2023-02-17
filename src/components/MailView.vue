04b0312933f64997fccbfa6c8950c5fabe79aeaa
Reniki <hello@reniki.com>
2023-02-17 18:59:29 +0100

    npm install marked, markdown for email.body

--- src/components/MailView.vue
+++ src/components/MailView.vue
@@ -2,12 +2,13 @@
     <div>
         <h2>Subject: <strong>{{ email.subject }}</strong></h2>
         <div>From {{ email.from }} on {{ format(new Date(email.sentAt), 'MMM do yyyy') }}</div>
-        <div>{{ email.body }}</div>
+        <div v-html="marked(email.body)"></div>
     </div>
 </template>
 
 <script setup>
 import { format } from 'date-fns'
+import { marked } from 'marked'
 
 const props = defineProps({
     email: Object
